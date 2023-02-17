d920ac97aa4b1308c63c485324d6ad9e5501139d
Fernando Laxi <fernandopegboard@gmail.com>
2023-02-17 02:13:17 -0300

    arregloApi2

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ const adv = document.getElementById('advice');
 const button = document.getElementById('btn');
 const num = document.getElementById('number');
 
-const apiAdvice = async () =>{
-    let url = ('https://api.adviceslip.com/advice', { cache: "no-store" })
-    await fetch (url)
+const apiAdvice = () =>{
+    let url = 'https://api.adviceslip.com/advice'
+    fetch (url)
     .then(res => res.json())
     .then(data => {
         num.innerText = `ADVICE #${data.slip.id}`
